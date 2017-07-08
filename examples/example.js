922939238df3e02ca2ed3f8b998e824e03d09149
serge1peshcoff <serge1peshcoff@mail.ru>
2017-07-08 22:47:05 +0300

    Working example

---
+++ examples/example.js
@@ -0,0 +1,53 @@
+// An example of using the new conditions.
+// Uses 'async/await', so Node.js >= 7.6.0
+
+const { Builder, By } = require('selenium-webdriver');
+const until = require('../index'); // Replace with 'selenium-until-extra' in real-life examples
+
+const sleep = delay => new Promise(res => setTimeout(() => res, delay));
+
+(async () => {
+	const driver = new Builder().forBrowser('firefox').build();
+
+  console.log('Testing \'until.urlIsNot\'...')
+  await driver.get('http://www.seleniumhq.org/projects/webdriver/');
+  await driver.findElement(By.css('#menu_download a')).click()
+  console.log('Waiting for \'urlIsNot\' condition...');
+  await driver.wait(until.urlIsNot('http://www.seleniumhq.org/projects/webdriver/'));
+  console.log('Finished waiting.');
+
+  console.log('Testing \'until.urlNotContains\'...')
+  await driver.get('http://www.seleniumhq.org/projects/webdriver/');
+  await driver.findElement(By.css('#menu_download a')).click()
+  console.log('Waiting for \'urlNotContains\' condition...');
+  await driver.wait(until.urlNotContains('webdriver'));
+  console.log('Finished waiting.');
+
+  console.log('Testing \'until.urlNotMatches\'...')
+  await driver.get('http://www.seleniumhq.org/projects/webdriver/');
+  await driver.findElement(By.css('#menu_download a')).click()
+  console.log('Waiting for \'urlNotMatches\' condition...');
+  await driver.wait(until.urlNotMatches(new RegExp('webdriver')));
+  console.log('Finished waiting.');
+
+  console.log('Testing \'until.titleIsNot\'...')
+  await driver.get('http://www.seleniumhq.org/projects/webdriver/');
+  await driver.findElement(By.css('#menu_download a')).click()
+  console.log('Waiting for \'titleIsNot\' condition...');
+  await driver.wait(until.titleIsNot('Selenium WebDriver'));
+  console.log('Finished waiting.');
+
+  console.log('Testing \'until.titleNotContains\'...')
+  await driver.get('http://www.seleniumhq.org/projects/webdriver/');
+  await driver.findElement(By.css('#menu_download a')).click()
+  console.log('Waiting for \'titleNotContains\' condition...');
+  await driver.wait(until.titleNotContains('WebDriver'));
+  console.log('Finished waiting.');
+
+  console.log('Testing \'until.titleNotMatches\'...')
+  await driver.get('http://www.seleniumhq.org/projects/webdriver/');
+  await driver.findElement(By.css('#menu_download a')).click()
+  console.log('Waiting for \'titleNotMatches\' condition...');
+  await driver.wait(until.titleNotMatches(new RegExp('WebDriver')));
+  console.log('Finished waiting.');
+})();
